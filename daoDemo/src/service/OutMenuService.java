811f0c267dca839f78e9d78285bbef00076b3b94
7color13 <61817428+7color13@users.noreply.github.com>
2020-05-19 23:04:36 +0800

    Update OutMenuService.java

--- daoDemo/src/service/OutMenuService.java
+++ daoDemo/src/service/OutMenuService.java
@@ -5,11 +5,12 @@ import view.OutMenuView;
 public class OutMenuService {   //输出相关的菜单
     public static boolean outFile() throws Exception{
         int choice = OutMenuView.printOutMenu();
+        OutFileService ofs = new OutFileService();
         while(choice!=3){
             switch (choice){
-                case 1:OutFileService.outputToExcel();
+                case 1:ofs.outputToExcel();
                     break;
-                case 2:OutFileService.outputToTxt();
+                case 2:ofs.outputToTxt();
                     break;
             }
             choice=OutMenuView.printOutMenu();
